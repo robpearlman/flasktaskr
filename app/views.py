dcb830a46300781a2de6d92a8183d9ee581f4544
Rob Pearlman <robpearlman@gmail.com>
2014-08-12 22:32:24 +1000

    ?blueprints is fucked

--- app/views.py
+++ app/views.py
@@ -1,141 +1,76 @@
-#views.py 
-# ---- imports and configuration ---- #
-from flask import Flask, flash, redirect, render_template, request, \
-session, url_for, g
+# views.py
+
+
+from app import app, db
+from flask import flash, redirect, render_template, session, url_for, request, jsonify, make_response
 from functools import wraps
-from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import IntegrityError
-from forms import AddTask, RegisterForm, LoginForm
-
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-from models import FTasks, User
-
-def login_required(test):
-	@wraps(test)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return test(*args, **kwargs)
-		else:
-			flash('You need to login first.')
-			return redirect(url_for('login'))
-	return wrap
-
-@app.route('/register/', methods=['GET', 'POST'])
-def  register():
-	error = None
-	form = RegisterForm(request.form, csrf_enabled=False)
-	if  form.validate_on_submit():	
-		new_user = User(
-			form.name.data,
-			form.email.data,
-			form.password.data,
-			)
-		try:
-			db.session.add(new_user)
-			db.session.commit()
-			flash('Thanks for registering. Please login.')
-			return redirect(url_for('login'))
-		except IntegrityError: #req sqlalch thing
-				error = 'Oh no! That username and/or email already exist. Please try again.'
-				return render_template('register.html', form=form, error=error)
-	else:
-		flash_errors(form)
-		print form.validate_on_submit()
-		print request.form
-		print form.data
-		return render_template('register.html', form=form, error=error)
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		u = User.query.filter_by(name=request.form['name'], password=request.form['password']).first()
-		if u is None:
-			error = 'Invalid username of password'
-		else:
-			session['logged_in'] = True
-			session['user_id'] = u.id #this is not what i expected
-			flash('You are logged in. Go Crazy.')
-			return redirect(url_for('tasks'))
-	return render_template('login.html', form=LoginForm(request.form), error=error)
-
-@app.route('/tasks/')
-@login_required #wrap
-def  tasks():
-	open_tasks = db.session.query(FTasks).filter_by(status='1').order_by(FTasks.due_date.asc())
-	closed_tasks = db.session.query(FTasks).filter_by(status='0').order_by(FTasks.due_date.asc())
-	return render_template('tasks.html',form = AddTask(request.form), open_tasks = open_tasks, closed_tasks = closed_tasks) 
-	# query db for open, closed tasks and pass them to tasks.html
-
-#add new tasks
-@app.route('/add/', methods=['GET', 'POST'])
-@login_required
-def  new_task():
-	form = AddTask(request.form, csrf_enabled=False)
-	print form.errors
-	if form.validate_on_submit():
-		#print form.errors
-		new_task = FTasks(
-			form.name.data, 
-			form.due_date.data, 
-			form.priority.data, 
-			form.posted_date.data,
-			'1',
-			session['user_id']
-			)
-		db.session.add(new_task)
-		db.session.commit()
-		flash('New entry successfully posted, thanks!')
-	else:
-		flash_errors(form)
-		print request.form
-		print request.data
-		#print form.errors
-		flash('unsuccessful. why?!')
-	return redirect(url_for('tasks'))
-
-#mark tasks as complete:
-@app.route('/complete/<int:task_id>/',)
-@login_required
-def complete(task_id):
-	new_id = task_id
-	db.session.query(FTasks).filter_by(task_id=new_id).update({"status":"0"})
-	db.session.commit()
-	flash('the task was marked as complete')
-	return redirect(url_for('tasks'))
-
-# delete tasks:
-@app.route('/delete/<int:task_id>/',)
-@login_required
-def delete_entry(task_id):
-	new_id = task_id
-	db.session.query(FTasks).filter_by(task_id=new_id).delete()
-	db.session.commit()
-	flash('the task was deleted')
-	return redirect(url_for('tasks'))
+from app.models import FTasks
 
 def flash_errors(form):
-	for  field, errors in form.errors.items():
-		for error in errors:
-			flash(u"Error in the %s field - %s" % ( getattr(form, field).label.text,error), 'error')
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,error), 'error')
 
-@app.errorhandler(500) 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('users.login'))
+    return wrap
+
+@app.errorhandler(500)
 def internal_error(error):
-	db.session.rollback()
-	return render_template('500.html'), 500
+    db.session.rollback()
+    return render_template('500.html'), 500
 
-@app.errorhandler(404) 
+@app.errorhandler(404)
 def internal_error(error):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
-@app.route('/logout/')
-def logout():
-	session.pop('logged_in', None)
-	session.pop('user_id', None)
-	flash('You are logged out. Bye!')
-	return redirect (url_for('login'))
+@app.route('/', defaults={'page': 'index'})
+def index(page):
+    return(redirect(url_for('users.login')))
+
+@app.route('/api/tasks/')
+def tasks():
+  if request.method == 'GET':
+    results = db.session.query(FTasks).limit(10).offset(0).all()
+    json_results = []
+    for result in results:
+      data = {
+            'task_id' : result.task_id,
+            'task name': result.name,
+            'due date': str(result.due_date),
+            'priority': result.priority,
+            'posted date': str(result.posted_date),
+            'status': result.status,
+            'user id': result.user_id
+            }
+      json_results.append(data)
+
+    return jsonify(items=json_results)
+
+@app.route('/api/tasks/<int:task_id>')
+def task(task_id):
+  if request.method == 'GET':
+    result = db.session.query(FTasks).filter_by(task_id=task_id).first()
+    if result:
+        json_result = {
+                'task_id' : result.task_id,
+                'task name': result.name,
+                'due date': str(result.due_date),
+                'priority': result.priority,
+                'posted date': str(result.posted_date),
+                'status': result.status,
+                'user id': result.user_id
+                }
+        result = json_result
+        code = 200
+    else:
+        result = {"sorry": "Element does not exist"}
+        code = 404
+    return make_response(jsonify(result), code)
--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 #run.py 
 
-from views import app
-app.run()#debug=True
+#from views import app
+#app.run()#debug=True
+
+# run.py
+
+
+import os
+
+from app import app
+app.run(debug=True)
+#port = int(os.environ.get('PORT', 5000))
+#app.run(host='0.0.0.0', port=port, debug=True) 
 
